85c80c9376ed455bd3052829cd1c1f32633c55f1
Desirae Zidan <zidan.desirae@gmail.com>
2020-03-15 21:58:00 -0700

    updated

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require("express");
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes")
+const htmlRoutes = require("./routes/htmlRoutes")
 
+// Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("public"))
 
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
 
-app.listen(PORT, () => console.log("App listening on PORT " + PORT));
+app.use(apiRoutes)
+app.use(htmlRoutes)
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
